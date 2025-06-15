967eafb044b53dd7cf06d143f294dceb2df00240
Ivan Krsnik <ivankrsnik93@gmail.com>
2025-06-15 15:15:40 +0100

    test: Card and Rank comparison

--- test/CardTest.hs
+++ test/CardTest.hs
@@ -8,6 +8,8 @@ tests :: Test
 tests =
   TestList
     [
-        TestLabel "Ace is higher than Two" (TestCase (assertBool "Ace should be higher than Two" (Card Ace Spade > Card Two Heart))),
-        TestLabel "Ace is equal to another Ace" (TestCase (assertBool "Ace should be equal to Ace of different Suit" (Card Ace Spade == Card Ace Heart)))
+        TestLabel "Ace is higher than Two" (TestCase (assertBool "Ace should be higher than Two" (Ace > Two))),
+        TestLabel "Kind is equal to King" (TestCase (assertBool "King should be equal to King" (King == King) )),
+        TestLabel "Ace of Spades is higher than Two of Hearts" (TestCase (assertBool "Ace of Spades should be higher than Two of Hearts" (Card Ace Spade > Card Two Heart))),
+        TestLabel "Three of Clubs is lower than Seven of Diamonds" (TestCase (assertBool "Three of Clubs should be lower than Seven of Diamonds" (Card Three Club < Card Seven Diamond)))
     ]
